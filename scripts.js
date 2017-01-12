d6e4007155ef31436579ba5e09d570f913cb3da9
Philneeves <p.neeves@gmail.com>
2017-01-12 17:45:37 +0000

    add in index.html, css folder and modified js

--- scripts.js
+++ scripts.js
@@ -4,23 +4,23 @@ switch(user) {
         var strong = prompt("Are you strong?").toUpperCase();
         var fit = prompt("Are you fit?").toUpperCase();
         if(strong === 'YES' || fit === 'YES') {
-            console.log("You rain down the blows, the troll goes down - you win!");
+            alert("You rain down the blows, the troll goes down - you win!");
         } else {
-            console.log("You are neither strong nor fit, either one would do, the troll beats the shit out of you!");
+            alert("You are neither strong nor fit, either one would do, the troll beats the shit out of you!");
         }
         break;
     case 'FLEE':
-        console.log("You shout 'What's over there!' and leg it! The troll loses you in the confusion");
+        alert("You shout 'What's over there!' and leg it! The troll loses you in the confusion");
         break;
     case 'PAY':
         var money = prompt("Do you have any money?").toUpperCase();
         var dollars = prompt("Do you have troll dollars?").toUpperCase();
         if(money === 'YES' && dollars === 'YES') {
-            console.log("You pull out your wallet and the troll sniggers as he empties out your last few troll dollars. He says 'Ha ha thanks, now you are free to go!")
+            alert("You pull out your wallet and the troll sniggers as he empties out your last few troll dollars. He says 'Ha ha thanks, now you are free to go!")
         } else {
-        console.log("The troll is enraged by your stupidity and sets about tearing you apart and steals all your clothes");
+        alert("The troll is enraged by your stupidity and sets about tearing you apart and steals all your clothes");
         }
         break;
     default:
-        console.log("The troll tilts his head, looks at you and punches you in the face saying 'No such thing loser!  Try again!'");
+        alert("The troll tilts his head, looks at you and punches you in the face saying 'No such thing loser!  Try again!'");
 }
